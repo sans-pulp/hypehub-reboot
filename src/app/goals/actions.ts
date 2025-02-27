31bf77f42de9dda90879377da75757f32c4243c6
Gloria <gloedem@gmail.com>
2025-02-26 23:11:52 -0500

    server action for goals

---
+++ src/app/goals/actions.ts
@@ -0,0 +1,18 @@
+'use server'
+import { insertGoal } from '@/db/queries/insert'
+import {InsertGoal} from '@/db/schema'
+// goal creation action
+export const createGoal = async(goal: InsertGoal) => {
+    try {
+    const newGoal = await insertGoal(goal)
+    return newGoal
+    } catch (error) {
+        console.error('Error creating goal:', error)
+        throw error
+    }
+}
+
+// goal completion action
+
+
+// experience points action
